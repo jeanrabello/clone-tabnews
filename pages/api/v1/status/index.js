12a918e78854291533a5a31b55c6d8c30b881ba0
Jean Rabello <jean.rabello.prog@gmail.com>
2024-12-29 22:33:49 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
-import database from "infra/database.js";
+import database from "infra/database";
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = (await database.query("SHOW server_version;")).rows[0]
+    .server_version;
+
+  const databaseMaxConnections = (await database.query("SHOW max_connections;"))
+    .rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseUsedConnections = (
+    await database.query({
+      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    })
+  ).rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseUsedConnections,
+      },
+    },
+  });
 }
 
 export default status;

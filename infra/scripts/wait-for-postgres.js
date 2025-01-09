09ff9e32503613f6416285d84b01294e32f64223
Jean Rabello <jean.rabello.prog@gmail.com>
2025-01-08 21:14:55 -0300

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres(params) {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n🟢 Postgres está pronto aceitando conexões\n");
+  }
+}
+
+process.stdout.write("\n\n🟡 Aguardando Postgres aceitar conexões");
+checkPostgres();
